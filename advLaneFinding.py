57f77a359b111458439672853f043bc3d7c8376c
eric van tassell <Eric.VanTassell@amd.com>
2018-09-30 12:09:50 -0500

    show pipe starting with path .vs. img

--- advLaneFinding.py
+++ advLaneFinding.py
@@ -14,14 +14,25 @@ def stage_fn_img_read(path):
      img = mpimg.imread(path)
      return img
 
-demo_pipeline = pipe.Pipe(
-    [pipe.PipeStage(stage_fn_img_read,
-                    {'name' : 'imgread', 'debug_level' : 2}),
-     pipe.PipeStage(stage_fn_stub,
-                    {'name' : 'stub1', 'debug_level' : 2}),
-     pipe.PipeStage(stage_fn_stub,
-                    {'name' : 'stub2', 'debug_level' : 2})
+demo_pipeline_1 = pipe.Pipe(
+    [
+        pipe.PipeStage(stage_fn_img_read,
+                       {'name' : 'imgread', 'debug_level' : 2}),
+        pipe.PipeStage(stage_fn_stub,
+                       {'name' : 'stub1', 'debug_level' : 2}),
+        pipe.PipeStage(stage_fn_stub,
+                       {'name' : 'stub2', 'debug_level' : 2})
      ],
     {'debug_level ': 3})
 
-demo_pipeline.exec('test_images/test1.jpg')
+demo_pipeline_2 = pipe.Pipe(
+    [
+        pipe.PipeStage(stage_fn_stub,
+                       {'name' : 'stub1', 'debug_level' : 2}),
+        pipe.PipeStage(stage_fn_stub,
+                       {'name' : 'stub2', 'debug_level' : 2})
+    ],
+    {'debug_level ': 3})
+
+demo_pipeline_1.exec('test_images/test1.jpg')
+demo_pipeline_2.exec(mpimg.imread('test_images/test1.jpg'))
