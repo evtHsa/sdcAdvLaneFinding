06eec33f5d390599663d0f941efe4807883d7aac
eric van tassell <Eric.VanTassell@amd.com>
2018-10-03 18:55:59 -0500

    mumble

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import pdb
+
+
 def ohBother(img):
         plt.figure()
         plt.imshow(img)
@@ -7,3 +10,14 @@ def ohBother_g(img):
         plt.figure()
         plt.imshow(img, cmap='Greys_r')
         plt.show()  
+
+def safeGetDictVal(dict, key):
+        try:
+                val = dict[key]
+        except KeyError:
+                val = None
+        return val
+
+def brk(msg=""):
+        print(msg + "\n\n")
+        pdb.set_trace()
