0b05ac572351bba46dca6d058f8394e859531c90
eric van tassell <Eric.VanTassell@amd.com>
2018-10-03 07:58:50 -0500

    wip

--- advLaneFinding.py
+++ advLaneFinding.py
@@ -6,9 +6,10 @@ import util as ut
 import ImgSaver as imgsvr
 import ImgViewer as imgvwr
 import ImRead as ir
+import cv2
+import pdb
 
-#gViewer = imgvwr.ImgViewer(4,4)
-gViewer = imgvwr.ImgViewer()
+gViewer = imgvwr.ImgViewer(w=4, h=4, rows=2, cols=2, title="advLaneFind")
 
 def stage_fn_stub(img):
     print("boy howdy")
@@ -23,7 +24,7 @@ def stage_fn_rgb2gray(img):
     return gray
 
 demo_pipeline_1 = pipe.Pipe(
-    [
+    [ 
         pipe.PipeStage(stage_fn_img_read,
                        {'name' : 'imgread', 'debug_level' : 2}),
         pipe.PipeStage(stage_fn_stub,
@@ -53,16 +54,6 @@ demo_pipeline_3 = pipe.Pipe(
 
 #demo_pipeline_1.exec('test_images/test1.jpg')
 #demo_pipeline_2.exec(ir.read('test_images/test1.jpg'))
-#demo_pipeline_3.exec('camera_cal/calibration1.jpg')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = 'curly')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = 'moe')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '1: sleepy')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '2: sneezy')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '3: doc')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '4: dopey')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '5: bashful')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '6: grumpy')
-#gViewer.push(ir.read('camera_cal/calibration1.jpg'), title = '7: happy')
 #
 
 #img = ir.read('camera_cal/calibration16.jpg')
@@ -71,5 +62,9 @@ demo_pipeline_3 = pipe.Pipe(
 
 chess_b_nx = 9 # per assignment overview
 chess_b_ny = 6 # per assignment overview
-#cal.calibrate_camera(gViewer, chess_b_nx, chess_b_ny)
-cal.calibrate_camera(None, chess_b_nx, chess_b_ny)
+ppd = demo_pipeline_3.get_parm_dict()
+ppd['cal_mtx'] , ppd['cal_dist'] = cal_mtx, cal_dist = cal.calibrate_camera(None,
+                                                                            chess_b_nx, chess_b_ny)
+raise Exception("add a stage to do undistort")
+demo_pipeline_3.exec('camera_cal/calibration1.jpg')
+
