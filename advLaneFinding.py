5cb9e29dbd978d89573569741e2af9a7c6f1ab26
eric van tassell <Eric.VanTassell@amd.com>
2018-10-04 08:15:19 -0500

    remove old stupid, costly pipe model

--- advLaneFinding.py
+++ advLaneFinding.py
@@ -4,37 +4,31 @@ import pipe
 import calibrate as cal
 import util as ut
 import ImgSaver as imgsvr
-import ImgViewer as imgvwr
-import pipe as pipe
+import ImgViewer as iv
 
-gViewer = imgvwr.ImgViewer(w=4, h=4, rows=2, cols=2, title="advLaneFind")
 
-demo_pipeline_3 = pipe.Pipe(
-    [
-        #@Undistort the image using cv2.undistort() with mtx and dist
-        #@Convert to grayscale
-        # Find the chessboard corners
-        # Draw corners
-        # Define 4 source points (the outer 4 corners detected in the chessboard pattern)
-        # Define 4 destination points (must be listed in the same order as src points!)
-        # Use cv2.getPerspectiveTransform() to get M, the transform matrix
-        # use cv2.warpPerspective() to apply M and warp your image to a top-down view
-        pipe.PipeStage(pipe.stage_fn_img_read,
-                       {'name' : 'imgread', 'debug_level' : 2}),
-        pipe.PipeStage(pipe.stage_fn_undistort,
-                       {'name' : 'undistort', 'debug_level' : 2, 'cmap' : 'Greys_r'}),
-        pipe.PipeStage(pipe.stage_fn_rgb2gray,
-                       {'name' : 'undistort', 'debug_level' : 2, 'cmap' : 'Greys_r'})
-    ],
-    {'debug_level ': 3, 'viewer' : gViewer})
+def lane_finding_take_1(path):
+    #@Undistort the image using cv2.undistort() with mtx and dist
+    #@Convert to grayscale
+    #@ Find the chessboard corners
+    # Draw corners
+    # Define 4 source points (the outer 4 corners detected in the chessboard pattern)
+    # Define 4 destination points (must be listed in the same order as src points!)
+    # Use cv2.getPerspectiveTransform() to get M, the transform matrix
+    # use cv2.warpPerspective() to apply M and warp your image to a top-down view
+    vwr = iv.ImgViewer(w=4, h=4, rows=2, cols=2, title="lane_finding_take1")
 
-chess_b_nx = 9 # per assignment overview
-chess_b_ny = 6 # per assignment overview
-ppd = demo_pipeline_3.get_parm_dict()
-ppd['cal_mtx'] , ppd['cal_dist'] = cal_mtx, cal_dist = cal.calibrate_camera(None,
-                                                                            chess_b_nx, chess_b_ny)
+    chess_b_nx = 9 # per assignment overview
+    chess_b_ny = 6 # per assignment overview
 
-demo_pipeline_3.exec('test_images/test1.jpg')
+    cal_mtx , cal_dist = cal_mtx, cal_dist = cal.calibrate_camera(None,
+                                                                  chess_b_nx, chess_b_ny)
+
+    tmp = ut.img_read(path, vwr)
+    tmp = ut.img_undistort(tmp, cal_mtx, cal_dist, vwr)
+    vwr.show()
+
+lane_finding_take_1('test_images/test1.jpg')
 print("FIXME: need to review get perspective transform and my code in the excercise")
 print("FIXME:specifically how I chose the poiints")
 
@@ -52,3 +46,4 @@ print("FIXME:specifically how I chose the poiints")
 
 
 #Note: When you apply a perspective transform, choosing four source points manually, as we did in this video, is often not the best option. There are many other ways to select source points. For example, many perspective transform algorithms will programmatically detect four source points in an image based on edge or corner detection and analyzing attributes like color and surrounding pixels.
+
