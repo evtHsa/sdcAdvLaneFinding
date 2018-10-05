d1a8bda34c9aa44460b141802d4c6836bd30a6bd
eric van tassell <Eric.VanTassell@amd.com>
2018-10-04 21:06:53 -0500

    alignment?

--- util.py
+++ util.py
@@ -29,38 +29,44 @@ def brk(msg=""):
         pdb.set_trace()
 
 def calibrate_camera(viewer, nx, ny):
-    # inputs:
-    #         viewer: for showing intermediate images, may be None
-    #         nx: number of corners in x dim of chessboard
-    #         ny: number of corners in y dim of chessboard
-    #
-    # outputs:
-    #         camera matrix
-    #         distortion coefficients
-    # see:
-    #  CV =https://docs.opencv.org/2.4.1/modules/calib3d/doc
-    #  CV/camera_calibration_and_3d_reconstruction.html#calibratecamera        
+        # inputs:
+        #         viewer: for showing intermediate images, may be None
+        #         nx: number of corners in x dim of chessboard
+        #         ny: number of corners in y dim of chessboard
+        #
+        # outputs:
+        #         camera matrix
+        #         distortion coefficients
+        # see:
+        #  CV =https://docs.opencv.org/2.4.1/modules/calib3d/doc
+        #  CV/camera_calibration_and_3d_reconstruction.html#calibratecamera        
 
-    objpoints = []
-    imgpoints = []
-    objp = np.zeros((nx*ny,3), np.float32)
-    objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1, 2)
-
-    for fname in glob.glob("camera_cal/*.jpg"):
-        tmp = iu.img_read(fname, None)
-        tmp = iu.img_rgb2gray(tmp, None)
-
-        ret, corners = cv2.findChessboardCorners(tmp, (nx, ny), None)
-        if ret:
-            objpoints.append(objp)
-            imgpoints.append(corners)
+        objpoints = []
+        imgpoints = []
+        objp = np.zeros((nx*ny,3), np.float32)
+        objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1, 2)
+        
+        for fname in glob.glob("camera_cal/*.jpg"):
+                print('FIXME: hey idiot are we supposed to calibrate on one img or all?')
+                print('FIXME: if all, how do we choose points like we did in excercise')
+                
+                
+                
+                tmp = iu.img_read(fname, None)
+                print("shape(%s) = %s" % (fname, tmp.shape))
+                tmp = iu.img_rgb2gray(tmp, None)
+                
+                ret, corners = cv2.findChessboardCorners(tmp, (nx, ny), None)
+                if ret:
+                        objpoints.append(objp)
+                        imgpoints.append(corners)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
                                                            tmp.shape[::-1],None,None)
 
-    #print("mtx = " + str(mtx))
-    #print("objp.shape = %s" % str(objp.shape))
-    return mtx, dist
+        #print("mtx = " + str(mtx))
+        #print("objp.shape = %s" % str(objp.shape))
+        return mtx, dist
 
 
 
