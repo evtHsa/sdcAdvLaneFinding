2c52bb35968a3a070d3d65a9ad6d8847a626c64a
eric van tassell <Eric.VanTassell@amd.com>
2018-10-07 10:49:41 -0500

    cleanup

--- advLaneFinding.py
+++ advLaneFinding.py
@@ -59,13 +59,12 @@ print("FIXME:specifically how I chose the poiints")
 #M = cv2.getPerspectiveTransform(src, dst)
 
 
-#Compute the inverse perspective transform:
-#Minv = cv2.getPerspectiveTransform(dst, src)
+    dir_sobel = iu.dir_thresh(gray,
+                              0.7, # FIXME: need new gpd['sobel_dir_thresh_min'] ?
+                              1.3, # FIXME: need new gpd['sobel_dir_thresh_min'] ?
+                              15, # FIXME: gpd['sobel_kernel_size'],
+                              gpd['sobel_out_depth'], vwr)
+    vwr.show()
 
 
-#Warp an image using the perspective transform, M:
-#warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-
-
-#Note: When you apply a perspective transform, choosing four source points manually, as we did in this video, is often not the best option. There are many other ways to select source points. For example, many perspective transform algorithms will programmatically detect four source points in an image based on edge or corner detection and analyzing attributes like color and surrounding pixels.
 
