b8dcf1adf415a23eae11d18ca3e605b60cc0caa5
eric van tassell <Eric.VanTassell@amd.com>
2018-10-09 15:51:59 -0500

    more demo stuff

--- advLaneFinding.py
+++ advLaneFinding.py
@@ -29,6 +29,8 @@ vwr = iv.ImgViewer(w=5, h=5, rows=2, cols=2, title="lane_finding_take1", svr=sav
 
 #lane_finding_take_1('test_images/signs_vehicles_xygrad.png')
 
-demo.doit_6_12('test_images/bridge_shadow.jpg', vwr=vwr)
+#demo.doit_6_12_hls('test_images/bridge_shadow.jpg', vwr=vwr)
+#demo.pipeline_6_12_mk2('test_images/bridge_shadow.jpg', cv2.COLOR_RGB2HLS, vwr=vwr)
+demo.pipeline_6_12_mk2('test_images/bridge_shadow.jpg', cv2.COLOR_RGB2HSV, vwr=vwr)
 
 vwr.show()
--- demo.py
+++ demo.py
@@ -78,14 +78,18 @@ def lane_finding_take_1(path):
 # see interesting things note in
 #
 # https://classroom.udacity.com/nanodegrees/nd013/parts/edf28735-efc1-4b99-8fbb-ba9c432239c8/modules/5d1efbaa-27d0-4ad5-a67a-48729ccebd9c/lessons/144d538f-335d-454d-beb2-b1736ec204cb/concepts/a1b70df9-638b-46bb-8af0-12c43dcfd0b4
-def pipeline_6_12(path, s_thresh=(170, 255), sx_thresh=(20, 100), vwr=None):
+def pipeline_6_12_hls(path, s_thresh=(170, 255), sx_thresh=(20, 100), vwr=None):
      
      img = iu.img_read(path, vwr)
      
       # Convert to HLS color space and separate the V channel
      hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+     h_channel = hls[:,:,0]
      l_channel = hls[:,:,1]
      s_channel = hls[:,:,2]
+     iv._push(vwr, np.squeeze(h_channel), "h_channel", cmap='gray')
+     iv._push(vwr, np.squeeze(l_channel), "l_channel", cmap='gray')
+     iv._push(vwr, np.squeeze(s_channel), "s_channel", cmap='gray') # shows lines well
      
      # Sobel x
      sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
@@ -109,8 +113,7 @@ def pipeline_6_12(path, s_thresh=(170, 255), sx_thresh=(20, 100), vwr=None):
 
 def doit_6_12(path, vwr=None):
 
-     image = iu.img_read(path, vwr)
-     result =pipeline_6_12(path, vwr=vwr)
+     result =pipeline_6_12_hls(path, vwr=vwr)
      # Plot the result
      f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
      f.tight_layout()
@@ -121,4 +124,40 @@ def doit_6_12(path, vwr=None):
      ax2.imshow(result)
      ax2.set_title('Pipeline Result', fontsize=40)
      plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+
+def pipeline_6_12_mk2(path, color_space_id, s_thresh=(170, 255),
+                       sx_thresh=(20, 100), vwr=None):
+     
+     img = iu.img_read(path, vwr)
+     
+      # Convert to HLS color space and separate the V channel
+     hls = cv2.cvtColor(img, color_space_id)
+     ch_0 = hls[:,:,0]
+     ch_1 = hls[:,:,1]
+     ch_2 = hls[:,:,2]
+     iv._push(vwr, np.squeeze(ch_0), "ch_0", cmap='gray')
+     iv._push(vwr, np.squeeze(ch_1), "ch_1", cmap='gray')
+     iv._push(vwr, np.squeeze(ch_2), "ch_2", cmap='gray') # shows lines well
+     
+     # Sobel x
+     sobelx = cv2.Sobel(ch_1, cv2.CV_64F, 1, 0) # Take the derivative in x
+     abs_sobelx = np.absolute(sobelx) # Abs x drvtv to accentuate lines away from horizontal
+     scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+     iv._push(vwr, np.squeeze(scaled_sobel), "scaled_sobel", cmap='gray')
+     ##
+     
+     # Threshold x gradient
+     sxbinary = np.zeros_like(scaled_sobel)
+     sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+     
+     # Threshold color channel
+     s_binary = np.zeros_like(ch_2)
+     s_binary[(ch_2 >= s_thresh[0]) & (ch_2 <= s_thresh[1])] = 1
+     iv._push(vwr, np.squeeze(s_binary), "s_binary", cmap='gray')
+     # Stack each channel
+     color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+     iv._push(vwr, np.squeeze(color_binary), "color_binary", cmap='gray')
+     ut.brk("look about")
+     return color_binary
 
