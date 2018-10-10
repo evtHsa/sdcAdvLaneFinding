9e0e7de1a6576dc816063138414290089bd5b166
eric van tassell <Eric.VanTassell@amd.com>
2018-10-10 18:36:00 -0500

    cleanup histo prototyping

--- histo.py
+++ histo.py
@@ -19,14 +19,15 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-# Load our image
-# `mpimg.imread` will load .jpg as 0-255, so normalize back to 0-1
-img = mpimg.imread('test_images/straight_lines1.jpg')/255
-print("img.shape = " + str(img.shape))
 
 saver = None
 vwr=None
-#vwr = iv.ImgViewer(w=5, h=5, rows=2, cols=2, title="lane_finding_take1", svr=saver)
+vwr = iv.ImgViewer(w=5, h=5, rows=2, cols=2, title="lane_finding_take1", svr=saver)
+
+img = iu.img_read('test_images/straight_lines1.jpg', vwr)
+print("img.shape = " + str(img.shape))
+gray = iu.img_rgb2gray(img, vwr)
+print("gray.shape = " + str(gray.shape))
 
 
 # for some bizzare reason, the image in the lesson, which I cant download,
@@ -36,22 +37,21 @@ def hist(img, vwr):
     # Lane lines are likely to be mostly vertical nearest to the car
     
     bottom_half = img[img.shape[0]//2:,:]
-    #iv._push(vwr, img, "orig")
-    #iv._push(vwr, bottom_half, "bottom half")
+    
     # TO-DO: Sum across image pixels vertically - make sure to set `axis`
     # i.e. the highest areas of vertical lines should be larger values
     histogram = np.sum(bottom_half, axis=0)
     print("bottom_half.shape = " + str(bottom_half.shape))
+    print("gray.shape = " + str(gray.shape))
     print("histogram.shape = " + str(histogram.shape))
-    #iv._push(vwr, histogram, "histogram")
-    
+    #iv._push(vwr, histogram, "")
     return histogram
 
 # Create histogram of image binary activations
-histogram = hist(img, vwr)
+histogram = hist(gray, vwr)
 #vwr.show()
 
 # Visualize the resulting histogram
 plt.plot(histogram)
+vwr.show()
 plt.show()
-print("wtf?")
