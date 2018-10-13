9165d172520a3ff7e9b889749ac7a91b9c751b4b
eric van tassell <Eric.VanTassell@amd.com>
2018-10-13 12:26:51 -0500

    knoingly committing brokem ImgSaver

--- ImgSaver.py
+++ ImgSaver.py
@@ -6,13 +6,12 @@ import os.path
 class ImgSaver:
     # abstracts saving a bunch of files in a timestamp directory
     def __init__(self, d="test_out/"):
+        raise Exception("FIXME: this is broken")
         d = d + dt.datetime.now().strftime("%a_%m%d%y_%H%M%S") + "/"
         self.outdir = d
         os.mkdir(d)
 
-    def save(self, img, img_name, cmap='Greys_r', msg=""):
-        outf = self.outdir + os.path.basename(img_name) + "_" + msg + ".png"
-        if cmap:
-            mpimg.imsave(outf, img, cmap=cmap)
-        else:
-            mpimg.imsave( outf, img)
+    def save(self, img_obj, msg=""):
+        outf = self.outdir + os.path.basename(imb_obj.title) + "_" + msg + ".png"
+        mpimg.imsave(outf, img, cmap=cmapimg_obj.cmap)
+
