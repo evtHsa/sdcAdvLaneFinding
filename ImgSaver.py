682421adfe2a61714e59bed7604f2b8e15bc9696
eric van tassell <Eric.VanTassell@amd.com>
2018-10-13 14:13:41 -0500

    fix the saver for new img objs

--- ImgSaver.py
+++ ImgSaver.py
@@ -2,16 +2,22 @@ import datetime as dt
 import os
 import matplotlib.image as mpimg
 import os.path
+import util as ut
+import ImgUtil as iu
+import cv2
 
 class ImgSaver:
     # abstracts saving a bunch of files in a timestamp directory
     def __init__(self, d="test_out/"):
-        raise Exception("FIXME: this is broken")
         d = d + dt.datetime.now().strftime("%a_%m%d%y_%H%M%S") + "/"
         self.outdir = d
         os.mkdir(d)
 
-    def save(self, img_obj, msg=""):
-        outf = self.outdir + os.path.basename(imb_obj.title) + "_" + msg + ".png"
-        mpimg.imsave(outf, img, cmap=cmapimg_obj.cmap)
+    def save(self, img, msg=""):
+        assert(type(img) is iu.Image)
+        img_data = img.img_data
+        if img.type == 'bgr':
+            img_data = cv2.cvtColor(img.img_data, cv2.COLOR_BGR2RGB)
+        outf = self.outdir + os.path.basename(img.title) + "_" + msg + ".png"
+        mpimg.imsave(outf, img_data, cmap=img.cmap)
 
