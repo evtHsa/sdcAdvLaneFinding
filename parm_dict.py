ae636d415be53e008929e037af797f24df4d7c38
eric van tassell <Eric.VanTassell@amd.com>
2018-10-13 14:16:17 -0500

    oops, parm_dict commit should be earlier

---
+++ parm_dict.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#in its own file so we can evolve it and share it with small unit tests
+
+import cv2
+
+# gpd -> global parm dict
+parm_dict ={ 
+    'camera_resolution' : (720, 1280), # rows, cols
+    'chessboard_nx' : 9,
+    'chessboard_ny' : 6,
+    'objpoints' : [],
+    'imgpoints': [],
+    'quick_calibrate': True,
+    'cal_mtx' : None,   # camera calibration matrix
+    'cal_dist': None,     # camera distortion coefficients
+    'sobel_min_thresh' : 30,
+    'sobel_max_thresh' : 100,
+    'sobel_kernel_size' : 3,
+    'sobel_out_depth' : cv2.CV_64F,
+    'hough_rho' : 3,
+    'hough_theta' : 0.2,
+    'hough_min_len' : 150,
+    'hough_max_gap' : 24,
+    'hough_min_angle_deg' : 25, # 30 - 5
+    'hough_max_angle_deg' : 65, # 60 + 5]
+    'hough_min_intersect' : 100
+}
+
+# add things here as psuedo documentation
+cache_dict = {
+    'mtx' : None, 
+    'dist' : None
+}
