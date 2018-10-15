fe3e60eb30422d1505387bf60f804437f9054681
eric van tassell <Eric.VanTassell@amd.com>
2018-10-15 18:47:58 -0500

    sliding windows, wip,1

--- sliding_windows.py
+++ sliding_windows.py
@@ -10,22 +10,24 @@ import numpy as np
 import ImgUtil as iu
 import matplotlib.pyplot as plt
 
-def analyze_hist(hist):
-    # see lesson 7.4
-    width = hist.img_data.shape[0]
-    midway = width // 2
-    
-    left_max_ix    = np.argmax(hist.img_data[0:midway])
-    right_max_ix = midway + np.argmax(hist.img_data[midway:width])
-    print("histo maxen = %d, %d" % (left_max_ix, right_max_ix))
-    
+def sliding_windows_pipe(path="", cd=None, pd=None, vwr=None):
+    # FIXME: subsume from tmp.py from 7.4 solution to here
+    wp = pd['sliding_windows']
+    nwindows, margin, minpix = (wp['nwindows'], wp['margin'], wp['minpix'])
+    img = iu.imRead(path, reader='cv2', vwr=None)
+    undistorted = iu.undistort(img, cd, vwr=None)
+    top_down = iu.look_down(undistorted, cd, vwr)
+    hls_lab = iu.hls_lab_lane_detect(top_down, cache_dict = cd, parm_dict = pd)
+    hist = iu.hist(hls_lab, vwr)
+    left_max_ix, right_max_ix = iu.get_LR_hist_max_ix(hist)
+    print("FIXME: histo maxen = %d, %d" % (left_max_ix, right_max_ix))
+    ut.brk("look what a fine mess you've got us into know ollie")
+  
 def doit(path="", cd=None, pd=None, vwr=None):
     vwr.flush()
     for path in ut.get_fnames("test_images/", "*.jpg"):
-        hist = iu.histo_pipe(path, cd, pd, vwr)
-        hist.title="hist: " + path
-        analyze_hist(hist)
-        iv._push(vwr, hist)
+        sliding_windows_pipe(path, cd, pd, vwr)
+        ut.brk('what ed koch said')
     vwr.show()
 
 cache_dict, parm_dict = ut.app_init(viewer=True, saver=False, title="whatever")
