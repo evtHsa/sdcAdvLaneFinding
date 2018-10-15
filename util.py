9cd5d42642444c35a52e7d70f636c527d2e8d642
eric van tassell <Eric.VanTassell@amd.com>
2018-10-15 01:14:32 -0500

    cleanup + import saver

--- util.py
+++ util.py
@@ -10,16 +10,7 @@ import hashlib
 import glob
 import camera
 import parm_dict as pd
-
-def ohBother(img):
-        plt.figure()
-        plt.imshow(img)
-        plt.show()
-        
-def ohBother_g(img):
-        plt.figure()
-        plt.imshow(img, cmap='Greys_r')
-        plt.show()  
+import ImgSaver as iS
 
 def existsKey(dict, key):
         try:
