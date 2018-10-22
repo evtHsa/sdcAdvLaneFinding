4619d7aa69fc7911eb088cbc50a9fedda2938fff
eric van tassell <Eric.VanTassell@amd.com>
2018-10-22 18:43:33 -0500

    remove old debug printf

--- ImgUtil.py
+++ ImgUtil.py
@@ -427,7 +427,6 @@ def get_LR_hist_max_ix(hist):
 def cv2Polylines(x_pts, y_pts, out_img, line_color=None, line_thickness=None):
     assert(type(out_img) is Image)
     pts = np.array(list(zip(x_pts, y_pts)), dtype=np.int32)
-    print("pts.shape = " + str(pts.shape))
     cv2.polylines(out_img.img_data, [pts], False, line_color, line_thickness)
 
 def get_binary_warped_image_v2(path="", cd=None, pd=None, vwr=None):
