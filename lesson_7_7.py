45524938ef13e169d90c0f9c83b9621968c52fee
eric van tassell <Eric.VanTassell@amd.com>
2018-10-25 09:15:06 -0500

    test my stuff with 7.7 fake data, step 2

--- lesson_7_7.py
+++ lesson_7_7.py
@@ -55,6 +55,11 @@ def my_way(ploty, left_fit, right_fit):
     init_img, binary_warped = iu.get_binary_warped_image_v2(path, cd, pd, vwr=None)
     # img is just to painlessly fake out Lane ctor
     lane = lu.Lane(cd, pd, img = init_img, vwr=None)
+    lane.ploty = ploty
+    lane.left_bndry = lu.LaneBoundary(0, # hope max ix doesnt matter for this test
+                                   binary_warped, 'L', lane=lane, vwr=None)
+    lane.right_bndry = lu.LaneBoundary(0, # hope max ix doesnt matter for this test
+                                    binary_warped, 'R', lane=lane, vwr=None)
     ut.brk("fake up lane & boundaries with fake data")
     
 def measure_curvature_pixels():
